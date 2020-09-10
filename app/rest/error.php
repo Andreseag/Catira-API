b1396d56802030b686ab7743329e28338663de8c
gitjaleman <javiersettex@gmail.com>
2020-09-10 17:58:43 -0500

    Project-Structure

--- app/rest/error.php
+++ app/rest/error.php
@@ -2,7 +2,7 @@
 header('content-type application/json '); 
 header('Access-Control-Allow-Origin: *');
 $obj=new conn;
-http_response_code(200);
+http_response_code(404);
   $return['success']=true;
   $return['message']="Ruta no encontrada";
   $return['sql']=null;
